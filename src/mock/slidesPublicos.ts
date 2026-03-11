008425c7cab9a4ebbdc67c7bfa6567cb9ed93e92
Christian Valentim <christianvalentim99@gmail.com>
2026-03-11 19:48:49 -0300

    :recycle: refator: deixando as imagens fixas

--- src/mock/slidesPublicos.ts
+++ src/mock/slidesPublicos.ts
@@ -1,30 +1,35 @@
+import consultar from '../assets/consultar.png';
+import consultarNegocios from '../assets/consultor-de-negocios.png';
+import preparacaoRelatorios from '../assets/preparacao-de-relatorios.png';
+import networking from '../assets/networking.png';
+
 export const slidesPublico = [
   {
     tituloCurto: "Empresários",
     tituloCompleto: "Empresários e empreendedores",
     descricaoCurta: "Para quem tem contato frequente com outros empresários e busca uma nova fonte de receita.",
     descricaoCompleta: "Que têm contato frequente com outros empresários, credores ou empresas com ativos judiciais e buscam uma nova fonte de receita estratégica.",
-    imagem: "https://lojaintegrada.com.br/hub//wp-content/uploads/2023/10/o-que-e-consultor-de-negocios-1024x683.jpg",
+    imagem: consultarNegocios,
   },
   {
     tituloCurto: "Advogados",
     tituloCompleto: "Advogados",
     descricaoCurta: "Para quem deseja indicar precatórios com segurança jurídica e preservação de honorários.",
     descricaoCompleta: "Que desejam indicar precatórios de seus clientes ou contatos, com segurança jurídica, preservação de honorários e total clareza no processo.",
-    imagem: "https://img.freepik.com/fotos-premium/preparacao-de-relatorios-de-testemunhas_1169648-147753.jpg", 
+    imagem: preparacaoRelatorios,
   },
   {
     tituloCurto: "Consultores",
     tituloCompleto: "Consultores",
     descricaoCurta: "Para profissionais que enxergam valor em indicações qualificadas de ativos.",
     descricaoCompleta: "Que atuam próximos a empresas ou pessoas físicas com créditos judiciais e enxergam valor em indicações qualificadas.",
-    imagem: "https://aicibrasil.org/wp-content/uploads/2024/05/im-1-1024x576.png",
+    imagem: consultar,
   },
   {
     tituloCurto: "Networking",
     tituloCompleto: "Networking",
     descricaoCurta: "Para quem prefere indicar para uma operação profissional, sem se envolver na negociação.",
     descricaoCompleta: "Que conhecem alguém que possui precatório e preferem indicar para uma operação profissional, sem se envolver diretamente na negociação.",
-    imagem: "https://png.pngtree.com/thumb_back/fh260/background/20220531/pngtree-elderly-gentleman-embracing-modern-technology-within-the-comfort-of-his-home-photo-image_30735076.jpg",
+    imagem: networking,
   }
 ];
